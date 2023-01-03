95cadbc7a2373c7ad7541b6477c46fdb7530b689
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-01-03 21:35:50 +0100

    Rdy for build

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -12,7 +12,8 @@ export default {
   },
   methods: {
     onClick() {
-      this.$emit("toggle-add-task")
+      // this.$emit("toggle-add-task")
+      this.$emit("btn-click")
     }
   }
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="green" />
+    <Button @btn-click="$emit('toggle-add-task')" v-bind:text="showAddTask ? 'Close' : 'Add Task'"
+      v-bind:color="showAddTask ? 'red' : 'green'" />
   </header>
 </template>
 
@@ -13,6 +14,7 @@ export default {
   name: 'Header',
   props: {
     title: String,
+    showAddTask: Boolean,
   },
   components: {
     Button,
