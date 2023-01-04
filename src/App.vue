860a6cc27dd8dcb1692494410bd26e5cc36fc1ee
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-01-04 21:40:43 +0100

    Finished

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
   <div class="container">
     <!-- 3. and then using component here -->
     <Header @toggle-add-task="toggleAddTask" title="Task Tracker" v-bind:showAddTask="showAddTask" />
-    <div v-show="showAddTask">
-      <AddTask @add-task="addTask" />
-    </div>
-    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" v-bind:tasks="tasks" />
+    <router-view v-bind:showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
@@ -14,97 +12,26 @@
 <script>
 // 1. Importing component
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask.vue'
+import Footer from './components/Footer'
+
 
 export default {
   name: 'App',
   components: {
     // 2. Registering the imported component
     Header,
-    Tasks,
-    AddTask,
+    Footer,
   },
   data() {
     return {
-      tasks: [],
       showAddTask: false,
     }
   },
   methods: {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
-    },
-    async deleteTask(id) {
-      if (confirm('Are you sure?')) {
-
-        const res = await fetch(`api/tasks/${id}`, {
-          method: 'DELETE',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify()
-        })
-
-        const data = res.json()
-
-        this.tasks = this.tasks.filter((task) => task.id !== id)
-      }
-    },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ?
-        { ...task, reminder: !task.reminder } : task)
-    },
-    async addTask(task) {
-      const res = await fetch('api/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(task)
-      })
-
-      const data = await res.json()
-
-      // this.tasks.push(newTask) detta m arg=newTask funkar också
-      this.tasks = [...this.tasks, data]
-    },
-    async fetchTasks() {
-      const res = await fetch("api/tasks");
-      const data = res.json();
-      return data;
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`);
-      const data = res.json();
-      return data;
     }
-  },
-  async created() {
-    this.tasks = await this.fetchTasks()
-  },
-  //   created() {
-  //     this.tasks = [
-  //       {
-  //         id: 1,
-  //         text: 'Öva på VUE',
-  //         day: 'Varje dag innan/efter lunch',
-  //         reminder: true,
-  //       },
-  //       {
-  //         id: 2,
-  //         text: 'Öva på react',
-  //         day: 'Varje kväll innan/efter middag',
-  //         reminder: true,
-  //       },
-  //       {
-  //         id: 3,
-  //         text: 'Skriv om CV',
-  //         day: 'Någon dag innan jan slut',
-  //         reminder: false,
-  //       },
-  //     ]
-  //   }
+  }
 }
 </script>
 
